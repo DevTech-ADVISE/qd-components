c5ed6db74260de20f5e99e13f18e4f9cbe7e9363
Andrew Blowe <tehandyb@gmail.com>
2015-09-09 18:08:29 -0400

    added audio dash component

---
+++ demo/src/audio-dash.demo.js
@@ -0,0 +1,43 @@
+var dc = require('../../index.js');
+var fixtures = require('../../spec/helpers/fixtures.js');
+
+require('dc/dc.css');
+
+var id, audioDash, data;
+var stateId, stateDimension, stateGroup, stateChart;
+var regionId, regionDimension, regionGroup, regionChart;
+
+data = crossfilter(fixtures.loadDateFixture());
+
+id = 'audio-dash';
+
+stateId = 'state-chart';
+
+regionId = 'region-chart';
+
+
+stateDimension = data.dimension(function(d) { return d.state; });
+stateGroup = stateDimension.group();
+
+regionDimension = data.dimension(function(d) { return d.region; });
+regionGroup = regionDimension.group();
+
+stateChart = dc.rowChart('#' + stateId);
+stateChart.dimension(stateDimension).group(stateGroup)
+  .width(600).height(200).gap(10)
+  .transitionDuration(0);
+
+regionChart = dc.rowChart('#' + regionId);
+regionChart.dimension(regionDimension).group(regionGroup)
+  .width(600).height(200).gap(10)
+  .transitionDuration(0);
+
+var audioCurrency = function(key, value) {return key + " " + qd.numberFormat(value) + " dollars";};
+var formatter = function(key, value) {return key + ": " + value};
+
+var audioDash = dc.audioDash("#audio-dash")
+  .charts({"Region": {chart: regionChart, formatter: formatter},
+           "State": {chart: stateChart, formatter: formatter}
+          });
+
+dc.renderAll();
