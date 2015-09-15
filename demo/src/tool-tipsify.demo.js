7a33b0be715c1b3e0184b60b7063ad68d19a3aac
Andrew Blowe <tehandyb@gmail.com>
2015-09-15 16:09:53 -0400

    added more charts to demo

--- demo/src/tool-tipsify.demo.js
+++ demo/src/tool-tipsify.demo.js
@@ -2,18 +2,21 @@ dc = require('../../index.js');
 var fixtures = require('../../spec/helpers/fixtures.js');
 
 require('dc/dc.css');
+require('./stylesheets/tool-tipsify.demo.scss');
 
 var data;
 var stateId, stateDimension, stateGroup, stateChart;
 var regionId, regionDimension, regionGroup, regionChart;
-var countryId, countryDimension, countryGroup, countryChart;
+var yearId, yearDimension, yearGroup, yearChart;
+var toolTipFunc = function(d) {return d.data.key + ": " + d.data.value;};
+var yearToolTipFunc = function(d) {return "Year: " + d.data.key + "<br/>Value: " + d.data.value}
 
 //add more charts, and show the tool tip positions, add any more documentation
 data = crossfilter(fixtures.loadDateFixture());
 
 stateId = 'state-chart';
 regionId = 'region-chart';
-countryId = 'country-chart';
+yearId = 'year-chart';
 
 stateDimension = data.dimension(function(d) { return d.state; });
 stateGroup = stateDimension.group();
@@ -21,20 +24,33 @@ stateGroup = stateDimension.group();
 regionDimension = data.dimension(function(d) { return d.region; });
 regionGroup = regionDimension.group();
 
-countryDimension = data.dimension(function(d) { return d.countrycode; });
-countryGroup = countryDimension.group();
+yearDimension = data.dimension(function(d) { return d.year; });
+yearGroup = yearDimension.group().reduceSum(function(d) { return d.value;});
 
 stateChart = dc.rowChart('#' + stateId);
 stateChart.dimension(stateDimension).group(stateGroup)
   .width(600).height(200).gap(10)
   .transitionDuration(0);
 
-regionChart = dc.rowChart('#' + regionId);
+regionChart = dc.pieChart('#' + regionId);
 regionChart.dimension(regionDimension).group(regionGroup)
-  .width(600).height(200).gap(10)
+  .width(600).height(200)
+  .radius(100)
+  .innerRadius(40)
+  .title(function(d) {return d.region})
   .transitionDuration(0);
 
-dc.toolTipsify(stateChart);
-dc.toolTipsify(regionChart, {position: 'n'});
+yearChart = dc.barChart('#' + yearId)
+  .dimension(yearDimension).group(yearGroup)
+  .width(600).height(200).gap(10)
+  .elasticY(true)
+  .x(d3.scale.ordinal().domain([2007, 2008, 2009, 2010, 2011]))
+  .xUnits(dc.units.ordinal);
+
+//*********tipsify your charts**************
+dc.toolTipsify(regionChart, {content: toolTipFunc});
+dc.toolTipsify(stateChart, {position: 'e', offset: [0, 5]});
+dc.toolTipsify(yearChart, {position: 'n', content: yearToolTipFunc, offset: [-5, 0]})
+
 
 dc.renderAll();
