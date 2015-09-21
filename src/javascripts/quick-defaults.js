92e6ca026460ce3cf58688f3e9e3207831e620dc
Andrew Blowe <tehandyb@gmail.com>
2015-09-21 15:30:37 -0400

    start of size box as dc mixin

---
+++ src/javascripts/quick-defaults.js
@@ -0,0 +1,35 @@
+var inflection = require('inflection'),
+  dtip = require('d3-tip')(d3);
+
+var quickDefaults = function(dc) {
+  
+  var original = {};
+
+  original.geoChoroplethChart = dc.geoChoroplethChart;
+  dc.geoChoroplethChart = function(parent, opts) {
+    var _chart = original.geoChoroplethChart(parent);
+    //Defaults for colors and data
+    var _colorRange = ["#a9c8f4", "#7fa1d2", "#5479b0", "#2a518e", "#002A6C"];
+    var _zeroColor = '#ccc';
+    var _colorDomain = [100, 60000];
+
+    _chart.colors(d3.scale.quantize().range(_colorRange))
+      .colorDomain(_colorDomain)
+      .colorCalculator(function (d) {
+        if(d === undefined) return _zeroColor;
+        if(d < 1 )return _zeroColor;
+        return _chart.colors()(Math.sqrt(d)); 
+      })
+      .projection(d3.geo.mercator())
+      .enableZoom(true)
+      .afterZoom(function(g, s){
+        g.selectAll('.country').selectAll('path').style('stroke-width',0.75 / s + 'px');
+      });
+
+    return _chart;
+  };
+
+  return dc;
+};
+
+module.exports = quickDefaults;
--- src/javascripts/size-boxify.js
+++ src/javascripts/size-boxify.js
@@ -7,10 +7,20 @@ require('font-awesome/css/font-awesome.css');
 require('../../src/stylesheets/common.scss');
 require('normalize.css/normalize.css');
 
-var sizeBoxify = function(chart) {
+var sizeBoxify = function(dc) {
+  var original = {};
+
+  original.rowChart = dc.rowChart;
+  dc.rowChart = function(parent, opts) {
+    var _chart = original.rowChart(parent);
+
+    _chart.sizeBoxify();
+
+    return _chart;
+  };
 
 
-
+  return dc;
 };
 
 module.exports = sizeBoxify;
