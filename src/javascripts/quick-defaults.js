50b366e5d94d3623acee26962ca86dda700f0fda
Andrew Blowe <tehandyb@gmail.com>
2015-10-14 14:52:10 -0400

    fix color scale for geochoropleth

--- src/javascripts/quick-defaults.js
+++ src/javascripts/quick-defaults.js
@@ -58,19 +58,30 @@ var quickDefaults = function() {
     //Defaults for colors and data
     var _colorRange = ["#a9c8f4", "#7fa1d2", "#5479b0", "#2a518e", "#002A6C"];
     var _zeroColor = '#ccc';
-    var _colorDomain = [100, 60000];
+
+    var colorDomainFunc = function() { 
+      return [d3.min(_chart.group().all(), function(d){return d.value}),
+     d3.max(_chart.group().all(), function(d){return d.value})];
+    };
+
+    var colorCalculatorFunc = function (d) {
+      if(d === undefined) return _zeroColor;
+      if(d < 1 )return _zeroColor;
+      return _chart.colors()(d); 
+    };
 
     _chart.colors(d3.scale.quantize().range(_colorRange))
-      .colorDomain(_colorDomain)
-      .colorCalculator(function (d) {
-        if(d === undefined) return _zeroColor;
-        if(d < 1 )return _zeroColor;
-        return _chart.colors()(Math.sqrt(d)); 
-      })
+      .colorCalculator(colorCalculatorFunc)
       .projection(d3.geo.mercator())
       .enableZoom(true)
       .afterZoom(function(g, s){
         g.selectAll('.country').selectAll('path').style('stroke-width',0.75 / s + 'px');
+      })
+      .on("preRender", function() {
+        _chart.colorDomain(colorDomainFunc());
+      })
+      .on("preRedraw", function() {
+        _chart.colorDomain(colorDomainFunc());
       });
 
     return _chart;
