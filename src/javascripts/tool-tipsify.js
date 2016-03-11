64a96ac54610e3272087fb03ecfc185471d97e94
Andrew Blowe <tehandyb@gmail.com>
2016-03-11 18:11:37 -0500

    added corrected scale for color legend on geochoropleth, also added tooltips to the color legend and the ability to add extra tooltips to charts for legends etc

--- src/javascripts/tool-tipsify.js
+++ src/javascripts/tool-tipsify.js
@@ -92,43 +92,118 @@ var addToolTipsifyToDc = function(){
 
 var toolTipsifyMixin = function(chart, tippableSelector){
 
+  var _legendToolTips = false;
+  var _extraToolTips = [];
+  var _position = 'mouse';
+  var _d3TipClass = 'd3-tip';
+  var _offset = [0, 0]; 
+  var _toolTipFormatter = d3.format(",");
+  var _toolTipContent = function(d) {
+    var label = (chart.label()(d) !== undefined) ? chart.label()(d) : chart.label()(d.data);
+    var value = (d.value !== undefined) ? d.value : d.data.value;
+    return "<label>" + label + "</label><br/>" + _toolTipFormatter(value);
+  };
+
+  chart.legendToolTips = function(_) {
+    if(!arguments.length) return _legendToolTips;
+    _legendToolTips = _;
+    chart.legendContent(function(legendable) { return legendable.label;});
+    _extraToolTips = [{content: chart.legendContent(), formatter: chart.legendFormatter(), selector: "g.dc-legend-item"}];
+    return chart;
+  };
+
+  chart.extraToolTips = function(_) {
+    if(!arguments.length) return _extraToolTips;
+    _extraToolTips = _;
+    return chart;
+  };
+  
+  chart.toolTipContent = function(_) {
+    if(!arguments.length) return _toolTipContent;
+    _toolTipContent = _;
+    return chart;
+  };
+
+  chart.toolTipFormatter = function(_) {
+    if(!arguments.length) return _toolTipFormatter;
+    _toolTipFormatter = _;
+    return chart;
+  };
+
   chart.toolTipsify = function(options){
     // default & override setup for formatter
-    var formatter = d3.format(",");
     if (options && options.formatter) {
-      formatter = options.formatter;
+      _toolTipFormatter = options.formatter;
     }
-
-    // default setup for content & position 
-    var content = function(d) {
-      var label = (chart.label()(d) !== undefined) ? chart.label()(d) : chart.label()(d.data);
-      var value = (d.value !== undefined) ? d.value : d.data.value;
-      return "<label>" + label + "</label><br/>" + formatter(value);
-    };
-    var position = 'mouse';
-    var offset = [0, 0]; 
 
     // override content and position via options
     if (options && options.content) {
-      content = options.content;
+      _toolTipContent = options.content;
     }
     if (options && options.position) {
-      position = options.position;
+      _position = options.position;
     }
     if(options && options.offset) {
-      offset = options.offset;
+      _offset = options.offset;
     }
     
-    var d3TipClass = 'd3-tip';
-    if (position === 'mouse') {
-      d3TipClass = 'd3-tip-mouse';
+    
+    if (_position === 'mouse') {
+      _d3TipClass = 'd3-tip-mouse';
     }
 
     chart.renderlet(function(){
       var ttId = chart.root().attr('id') + '-tip';
 
       var tt = d3.tip()
-        .attr('class', d3TipClass)
+        .attr('class', _d3TipClass)
+        .attr('id', ttId)
+        .html(_toolTipContent)
+        .offset(_offset);
+
+      if (_position === 'mouse') {
+        tt.positionAnchor(_position)
+      }
+      else {
+        tt.direction(_position);
+      }
+
+      var tippables = chart.selectAll(tippableSelector); 
+
+      // HACK...
+      if (!d3.select('#' + ttId).empty()) d3.select('#' + ttId).remove(); 
+
+      tippables.call(tt);
+      tippables.on('mouseover', tt.show)
+               .on('mouseout', tt.hide);
+      if(_position === 'mouse') {
+        tippables.on('mousemove', tt.updatePosition);
+      }
+
+      //Add any extra tool tips
+      if(_legendToolTips === true || Object.keys(_extraToolTips).length > 0) {
+        chart.addExtraToolTips();
+      }
+    });
+  };
+
+  chart.addExtraToolTips = function() {
+    _extraToolTips.forEach(function(t, index) {
+      var position = t.position || _position;
+      var offset = t.offset || _offset;
+      var selector = t.selector;
+      var formatter = t.formatter || _toolTipFormatter;
+      var content = t.content || function(d) {
+        var label = (chart.label()(d) !== undefined) ? chart.label()(d) : chart.label()(d.data);
+        var value = (d.value !== undefined) ? d.value : d.data.value;
+        return "<label>" + label + "</label><br/>" + formatter(value);
+      } || _toolTipContent;
+
+      //Add unique id for tooltip
+      var ttId = chart.root().attr('id') + '-tip-' + index;
+
+      var tt = d3.tip()
+        .attr('class', _d3TipClass)
         .attr('id', ttId)
         .html(content)
         .offset(offset);
@@ -140,7 +215,7 @@ var toolTipsifyMixin = function(chart, tippableSelector){
         tt.direction(position);
       }
 
-      var tippables = chart.selectAll(tippableSelector); 
+      var tippables = chart.selectAll(selector); 
 
       // HACK...
       if (!d3.select('#' + ttId).empty()) d3.select('#' + ttId).remove(); 
